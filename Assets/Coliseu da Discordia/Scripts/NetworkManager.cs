a7c814b1e887af86269ac1cf94e7b9207f348dc4
Diego Garcia <diegogarcia.eng@gmail.com>
2024-05-02 19:01:28 -0300

    First Commit

---
+++ Assets/Coliseu da Discordia/Scripts/NetworkManager.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+
+public class NetworkManager : MonoBehaviourPunCallbacks
+{
+
+    private void Start()
+    {
+        PhotonNetwork.ConnectUsingSettings();    
+    }
+
+    public override void OnConnectedToMaster()
+    {
+        Debug.Log("OnConnectedToMaster");
+        PhotonNetwork.JoinLobby();
+    }
+
+    public override void OnJoinedLobby()
+    {
+        Debug.Log("OnJoinedLobby");
+        PhotonNetwork.CreateRoom("Game");
+    }
+
+
+    public override void OnCreatedRoom()
+    {
+        Debug.Log("OnCreatedRoom");
+        
+    }
+}
